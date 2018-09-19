2a9ff97ef11735c600b8b2e6fd764f95aaa4dee4
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-19 23:48:01 +0900

    Create shop model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_19_050347) do
+ActiveRecord::Schema.define(version: 2018_09_19_143805) do
+
+  create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address"
+    t.integer "phone", limit: 1, null: false
+    t.string "email"
+    t.string "image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
