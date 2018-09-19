0cee556ea08874138710d96dd07abb7818d1891a
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-20 00:08:04 +0900

    add route for search on shops controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   delete '/logout', to:'sessions#destroy'
   resources :users, only: [:new, :create]
   # new/createアクションへのルート設定　
+  get '/shops/search', to: 'shops#search'
+  # scaffoldにはsearchが含まれていない為、別途定義
   resources :shops
   # index/show/new/create/edit/update/destroyアクションへのルート設定
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
