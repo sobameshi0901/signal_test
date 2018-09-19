aaeb8f3890544db0e3a3c2957ac480f975c4984e
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-19 17:04:49 +0900

    User create function on controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,24 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user  = User.new(user_params)
+    # Userクラスのインスタンスを作成。引数にuser_paramsを入れることで、user_paramsのデータ(permitされたフォームから送られた情報)をインスタンスの変数として登録
+    if @user.save
+      # 上で作成して情報を登録したインスタンスをsaveメソッドでDBに保存。このDBで保存する際に、has_secure_passwordによってパスワードはハッシュ化される。
+    else
+      render 'new'
+      # 保存が失敗した場合には、エラーメッセージを表示。
+      # エラーメッセージはuser.erros.full_messageオブジェクトに配列で保存される
+      # renderはインスタンスの情報をそのままViewに渡すので、エラーメッセージはビューでそれを取り出す処理をしておけば良い。
+    end
   end
+
+  private
+  # このクラス内部からのみ使用可能なprivateメソッドの定義
+    def user_params
+      params.require(:user).permit(:email, :password)
+      # ストロングパラメータで、送られてきたパラメータのうち必要な:emailと:passwordのみを許可、ハッシュ型で取得
+      # まずはrequireで:userを引数にすることで、パラメータのうち、:userというキーに入ったハッシュだけを返り値として取得
+      # そのうちで登録に必要な:emailと:passwordを引数に、permitメソッドを利用し、{'email: 'ユーザー入力値', 'password: 'ユーザー入力値'}という形で帰ってくる。
+    end
 end
