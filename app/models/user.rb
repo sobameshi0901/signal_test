084715e9c66aa0c0609c60c3873fdb1a1e69c1bd
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-19 15:12:08 +0900

    Password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,16 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /[\w_-]+@[\w_-]+\.[\w_-]/
   # emailの形式確認用定数。半角英数字(大文字含)、"_"、"\"、"."のいずれかで@を挟んでいればOK
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
-  # バリデーション用メソッド。引数に(:email, presence: true, format: {with VALID_EMAIL_REGEX})をとる。
+  # バリデーション用メソッド。引数に(:email, presence: true, format: {with VALID_EMAIL_REGEX}, uniqueness: true)をとる。
   # レコードを作成・編集する際に
   # (1)emailが入力されていない
   # (2)emailがVALID_EMAIL_REGEXの型になっていない
   # (3)既に同じ emailが存在する
   # のいずれかに当てはまるとfalseが帰ってきて、反映がされない。
+
+  VALID_PASS_REGEX = /[\w]+/
+  validates :password, presence: true, format: {with: VALID_PASS_REGEX}
+  # 基本機能はemailと同じ
 
   has_secure_password
   # 以下の機能が追加される
