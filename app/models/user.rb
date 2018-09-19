f1fd323604e5cc255c5cd199359d8a5664d7c69a
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-19 14:59:50 +0900

    Revise Email format Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = ^[a-zA-Z0-9-_\.]+@[a-zA-Z0-9-_\.]+$
+  VALID_EMAIL_REGEX = /[\w_-]+@[\w_-]+\.[\w_-]/
   # emailの形式確認用定数。半角英数字(大文字含)、"_"、"\"、"."のいずれかで@を挟んでいればOK
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, unique: true
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
   # バリデーション用メソッド。引数に(:email, presence: true, format: {with VALID_EMAIL_REGEX})をとる。
   # レコードを作成・編集する際に
   # (1)emailが入力されていない
