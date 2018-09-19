a84765eae149c94c9b62f5aa4bff939185e5566a
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-19 15:05:01 +0900

    Add method has_secure_password for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,9 @@ class User < ApplicationRecord
   # (3)既に同じ emailが存在する
   # のいずれかに当てはまるとfalseが帰ってきて、反映がされない。
 
-
+  has_secure_password
+  # 以下の機能が追加される
+  # ユーザーが入力したパスワードをハッシュ化してDB内password_digestに保存
+  # 仮想的な属性 (passwordとpassword_confirmation) が使えるようになる。これらの一致するかどうかのバリデーションも追加される
+  # authenticateメソッドが使えるようになる(引数の文字列がパスワードと一致するとUserオブジェクトを、間違っているとfalseを返すメソッド)
 end
