d110f560ce309d9900b0bd6e11c526ee81f571b6
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-19 21:21:42 +0900

    Add Session helper current_user and logged_in?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,10 @@ class SessionsController < ApplicationController
     # フォームから送られてきた情報(email)を引数にfind_byメソッドを行い、返ってきたUserのインスタンスをuserに代入
     if user && user.authenticate(params[:session][:password])
     # userが存在する（フォームのemailアドレスを持ったユーザーがいる）かつ
-    # passwordが正しければ(フォームに入力されたpasswordを引数にauthenticateメソッドでハッシュ化し、userのpasswordと照合すれば)trueが返されif文はtrueとなる
+    # passwordが正しければ(フォームに入力されたpasswordを引数にauthenticateメソッドでハッシュ化し、userのpasswordと照合すれば)trueが返されif文はtrueとなる 
+      log_in user
+      # sessionのヘルパーメソッドlog_inをuserを引数に実行。session[:user_id]にuser.idが保存される
+      redirect_to new_user_url
     else
       flash.now[:danger] = 'メールアドレスとパスワードの組み合わせが一致しません'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,17 @@ module SessionsHelper
     session[:user_id] = user.id
     # railsで定義されたsessionメソッドに引数のuserインスタンスのidを保存
   end
+
+  # 現在ログイン中のユーザーを返す
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+      # session[:user_id]がDB内のUserのidと一致すれば@current_userにそのインスタンスを返す。いなければnilを返す
+    end
+  end
+
+  # ユーザーがログインして入ればtrue、その他はfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
