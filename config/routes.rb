175f25d689fe1f78ab922768c681e0423bbad77e
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-19 23:57:21 +0900

    Set route for shops controller by scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'shops/index'
-  get 'shops/show'
-  get 'shops/new'
-  get 'shops/edit'
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
   resources :users, only: [:new, :create]
+  # new/createアクションへのルート設定　
+  resources :shops
+  # index/show/new/create/edit/update/destroyアクションへのルート設定
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
