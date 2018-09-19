cc6fa0ea5508971ebbb3825a7da3566da0412184
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-19 21:05:38 +0900

    Define log_in method on Session Helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    # フォームから送られてきた情報(email)を引数にfind_byメソッドを行い、返ってきたUserのインスタンスをuserに代入
+    if user && user.authenticate(params[:session][:password])
+    # userが存在する（フォームのemailアドレスを持ったユーザーがいる）かつ
+    # passwordが正しければ(フォームに入力されたpasswordを引数にauthenticateメソッドでハッシュ化し、userのpasswordと照合すれば)trueが返されif文はtrueとなる
+    else
+      flash.now[:danger] = 'メールアドレスとパスワードの組み合わせが一致しません'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+    # railsで定義されたsessionメソッドに引数のuserインスタンスのidを保存
+  end
 end
