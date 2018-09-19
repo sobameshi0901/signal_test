9a775c0d00cb8c84ca966e10ab14c4014e037ee3
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-19 23:54:18 +0900

    Create shops controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'shops/index'
+  get 'shops/show'
+  get 'shops/new'
+  get 'shops/edit'
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
