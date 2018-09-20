788f6ab1c15b5924ec742ec262f77b6ebc113f17
sobameshi0901 <taptaptap0901@yahoo.co.jp>
2018-09-21 02:22:06 +0900

    Add image preview function by jquery

--- app/assets/javascripts/shop.js
+++ app/assets/javascripts/shop.js
@@ -1,1 +1,15 @@
-shop.js
+$(function() {
+ function readURL(input) {
+   if (input.files && input.files[0]) {
+     var reader = new FileReader();
+     reader.onload = function (e) {
+       $('.reviewImage').attr('src', e.target.result);
+       $('.trimImage').css('display','block')
+     }
+     reader.readAsDataURL(input.files[0]);
+   }
+ }
+ $("#shop_image").change(function(){
+   readURL(this);
+ });
+});
